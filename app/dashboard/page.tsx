81e67cb414e430d406fb98f9d99b6181f9f43631
MandelRealty <mandelrealtyteam@gmail.com>
2025-12-14 18:38:40 -0500

    update dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 
 export default async function DashboardPage() {
-  const supabase = createClient();
+  const supabase = await createClient();
 
   const {
     data: { user },
